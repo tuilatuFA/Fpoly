f2b2ef7e8e296aa169b73b84a7b91dee15571450
tuilatuFA <attu5258@gmail.com>
2022-10-08 19:46:44 +0700

    first commit

---
+++ PS18411_PhamPhanDuyTu_ASM_GD3/db/Students.js
@@ -0,0 +1,32 @@
+﻿[
+    {
+        "username": "tudeptrai",
+        "password": "123",
+        "fullname": "Phạm Phan Duy Tú",
+        "email": "tudt@fpt.edu.vn",
+        "gender": "true",
+        "birthday": "2002-09-02",
+        "schoolfee": "1500000",
+        "marks": "0"
+    },
+    {
+        "username": "pheonv",
+        "password": "iloveyou",
+        "fullname": "Nguyễn Văn Chí Phèo",
+        "email": "pheonv@fpt.edu.vn",
+        "gender": "true",
+        "birthday": "1985-10-11",
+        "schoolfee": "2500000",
+        "marks": "0"
+    },
+    {
+        "username": "nopt",
+        "password": "iloveyou",
+        "fullname": "Phạm Thị Nở",
+        "email": "nopt@fpt.edu.vn",
+        "gender": "false",
+        "birthday": "1993-05-15",
+        "schoolfee": "2000000",
+        "marks": "0"
+    }
+]
